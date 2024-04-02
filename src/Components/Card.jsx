1183f16d142774d77107a3ddfbc301b5aa1683e2
AdhiRaj08 <113873429+AdhiRaj08@users.noreply.github.com>
2024-04-02 16:24:13 +0530

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import TaskBtn from './TaskBtn';
 import '../App.css';
 
-function Card (props) {
+function Card(props) {
+  // Render the Card component
   return (
-    <div className='Card' style = {{backgroundColor:props.complete === true && 'gray', color:props.complete === true && 'white'}}>
-        <h3> {props.title} </h3>
-        <p> {props.des} </p>
-        <div>
-          <TaskBtn color = 'white' bg = '#1877F2' name = 'Complete' click = {props.update} />
-          <TaskBtn color = 'white' bg = 'red' name = 'Delete' click = {props.delete} />
-        </div>
+    <div className='Card' style={{ backgroundColor: props.complete === true && 'gray', color: props.complete === true && 'white' }}>
+      {/* Display task title */}
+      <h3> {props.title} </h3>
+      
+      {/* Display task description */}
+      <p> {props.des} </p>
+      
+      {/* Button section */}
+      <div>
+        {/* Button to mark task as complete */}
+        <TaskBtn color='white' bg='#1877F2' name='Complete' click={props.update} />
+        
+        {/* Button to delete task */}
+        <TaskBtn color='white' bg='red' name='Delete' click={props.delete} />
+      </div>
     </div>
   );
 }
