57355e38ea5e7637d3ee5917362801015a6f08e0
AdhiRaj08 <113873429+AdhiRaj08@users.noreply.github.com>
2024-04-02 16:24:50 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,19 +2,28 @@ import React from 'react';
 import TaskBtn from './TaskBtn';
 import '../App.css';
 
-function Header (props) {
+function Header(props) {
+  // Render the Header component
   return (
     <>
-    <div className='header'>
+      {/* Header section */}
+      <div className='header'>
+        {/* Title */}
         <h1> Task Board </h1>
+        
+        {/* User logo */}
         <div>
-            <img src={require('../user_logo.png')} alt="user_logo" style = {{borderRadius:'14px', height:'2em'}}/>
+          <img src={require('../user_logo.png')} alt="user_logo" style={{ borderRadius: '14px', height: '2em' }} />
         </div>
-    </div>
-    <section>
-        <TaskBtn click = {props.handleInput} name = 'Add New Task!!' bg = '#1877F2' color = 'white'></TaskBtn>
-    </section>
+      </div>
+      
+      {/* Section for adding new task */}
+      <section>
+        {/* Button to toggle input fields */}
+        <TaskBtn click={props.handleInput} name='Add New Task!!' bg='#1877F2' color='white'></TaskBtn>
+      </section>
     </>
   )
 }
+
 export default Header;
